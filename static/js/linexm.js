555d1fcfe6afd09b8f975374496ba1cec29afda6
kxity <kxity@outlook.com>
2020-12-14 17:01:10 +0800

    2

---
+++ static/js/linexm.js
@@ -0,0 +1,11 @@
+ var arr_wx =["wa666999"]; //创建line号
+	 		var kfhlja =["https://line.me/ti/p/XlgXdVfw5p"]; //创建line号的链接
+	    var wx_index = Math.floor((Math.random() * arr_wx.length)); 
+	    var stxlwx = arr_wx[wx_index];
+	 		var kfhljas = kfhlja[wx_index];
+	 		 
+	 function showline() { 
+	  
+  window.open(kfhljas);
+	    fbq('track', 'Contact');
+	  }
